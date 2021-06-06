31bc676a52e82089f944ed7307b5a3ace7227865
Padmini Rai <59466080+PadminiRai@users.noreply.github.com>
2021-06-06 15:42:53 +0530

    Update urls.py

--- main_employee_project/employee_register/urls.py
+++ main_employee_project/employee_register/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path,include
 from . import views
+from .views import *
 urlpatterns = [
     path('',views.employee_form ),
     path('list/',views.employee_list ),
+    path('safety_ohs_a9/',safety_ohs_a9,name='safety_ohs_a9'),
+    path('homepagef/',homepagef,name='homepagef'),
+    path('charityregisterf/',charityregisterf,name='charityregisterf'),
+    path('editschemef/',editschemef,name='editschemef'),
+    path('acceptf/',acceptf,name='acceptf'),
+   
 ]
